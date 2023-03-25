4de910502a14635748f01d183ebde0fd8946ed9f
goodjun <goodjun@163.com>
2023-03-25 20:47:48 +0800

    refactor: refactor dropdown list

--- src/Component.svelte
+++ src/Component.svelte
@@ -1,6 +1,10 @@
 <script>
+  import { clickOutside } from "./ClickOutside.js";
   import { getContext } from "svelte";
-  import OutClick from "svelte-outclick";
+
+  export let variant;
+  export let size;
+  export let direction;
 
   let active = false;
 
@@ -9,11 +13,13 @@
 </script>
 
 <div use:styleable={$component.styles}>
-  <OutClick on:outclick={() => (active = false)}>
+  <div use:clickOutside on:click_outside={() => (active = false)}>
     <button
       on:click={() => (active = !active)}
       type="button"
-      class="spectrum-Button spectrum-Button--outline spectrum-Button--primary spectrum-Button--sizeM"
+      class="spectrum-Button 
+      spectrum-Button--{variant ?? 'primary'} 
+      spectrum-Button--size{size ?? 'M'}"
     >
       <svg
         class="spectrum-Icon spectrum-UIIcon-ChevronDown100 spectrum-Accordion-itemIndicator"
@@ -21,19 +27,28 @@
         aria-hidden="true"
       >
         <use xlink:href="#spectrum-css-icon-Chevron100" />
-      </svg></button
-    >
+      </svg>
+    </button>
 
     {#if active === true}
-      <div class="dropdown-memnu-container">
-        <slot />
+      <div class="dropdown-memnu-container" style:flex-direction={direction}>
+        {#if !!$$slots['slot']}
+          <div>
+            <h2>Custom content:</h2>
+            <slot name="slot" />
+          </div>
+        {:else}
+          <span>Item not found.</span>
+        {/if}
       </div>
     {/if}
-  </OutClick>
+  </div>
 </div>
 
 <style>
   .dropdown-memnu-container {
+    display: flex;
+    gap: 4px;
     z-index: 9999;
     position: absolute;
     background-color: white;
